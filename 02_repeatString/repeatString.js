590f7709bf313216cb1b3ffeacea702b726c41e7
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-04 22:33:03 +0800

    02 js repeatString Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatCount) {
+    if(repeatCount < 0) return "ERROR";
+    
+    let repeatedText = "";
+    for(let i = 0; i < repeatCount; i++){
+        repeatedText += text;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
