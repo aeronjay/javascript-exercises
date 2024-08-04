a0898e9ceda25ee21f76c1bb4a3699be59dc3711
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-04 22:39:51 +0800

    03 reversedString js done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textLen = text.length;
+    let reversedText = ""
+    for(let i = textLen - 1; i >= 0; --i){
+        reversedText += text[i];
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
