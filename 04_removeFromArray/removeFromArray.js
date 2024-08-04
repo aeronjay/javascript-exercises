344d7c4ddd644de30b2758d67f9daba58218a31f
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-04 23:46:38 +0800

    Done 04 js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    let newArr = [];
+    array.forEach((i) => {
+        if(!(items.includes(i))){
+            newArr.push(i);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
