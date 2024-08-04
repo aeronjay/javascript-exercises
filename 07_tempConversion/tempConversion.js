15eb3544733a390d66af5149ab874403ba0a5f6d
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-05 01:13:17 +0800

    done 07 js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let C = ((F - 32) * 5) / 9;
+
+  if(Number.isInteger(C)){
+    return parseInt(C);
+  }else{
+    return parseFloat(C.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  
+  let F = ((C * 9) / 5) + 32;
+
+  if(Number.isInteger(F)){
+    return parseInt(F);
+  }else{
+    return parseFloat(F.toFixed(1));
+  }
 };
 
 // Do not edit below this line
