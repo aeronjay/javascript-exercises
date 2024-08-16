15522152ec0c44e227d0607cdbeca3a5d161bca5
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-17 03:28:51 +0800

    done 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let alpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let removedPunc = text.toLowerCase().split("").filter((i) => alpha.includes(i)).join("");
+
+    let reversed = removedPunc.split("").reverse().join("");
+    return removedPunc === reversed;
 };
 
 // Do not edit below this line
