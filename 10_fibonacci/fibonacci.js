5b70c094e8d5ae54a7b7540fd629396ea4b5e4bb
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-17 04:00:25 +0800

    done 10 fibo

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num === 0 || num === "0") return 0;
+    let fiboNums = [1,1];
+    for(let i = 2; i <= num; i++){
+        fiboNums.push(fiboNums[i - 1] + fiboNums[i - 2])
+    }
+    return fiboNums[num - 1];
 };
 
 // Do not edit below this line
