46375f280f76da458a4c01da1779b45f3c894a2f
Aeron Jay Bulatao <bulataoaeronjay@gmail.com>
2024-08-17 04:50:51 +0800

    done 12 last

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(element => {
+        if(element.yearOfDeath === undefined){
+            element.yearOfDeath = 2024;
+        }
+        element.age = element.yearOfDeath - element.yearOfBirth;
+    });
+    let oldest = people[0];
 
+    people.forEach((person, index) => {
+        if(index >= 1){
+            if(person.age > oldest.age){
+                oldest = person;
+            }
+        }
+    })
+    return oldest;
+    
 };
 
 // Do not edit below this line
